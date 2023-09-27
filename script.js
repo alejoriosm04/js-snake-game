aba13dbf8e72650a9e43ec2da5653be7250e481d
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2023-09-27 09:09:48 -0500

    Show game over alert when snake collides with wall and avoid going backwards

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 const playBoard = document.querySelector(".play-board");
 
+let gameOver = false;
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
 let snakeBody = [];
 let velocityX = 0, velocityY = 0;
+let setIntervalId;
 
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+const handleGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over! Press OK to replay...");
+    location.reload();
+}
+
 const changeDirection = (event) => {
-    if(event.key === "ArrowUp") {
+    if(event.key === "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    } else if (event.key === "ArrowDown") {
+    } else if (event.key === "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if (event.key === "ArrowLeft") {
+    } else if (event.key === "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if (event.key === "ArrowRight") {
+    } else if (event.key === "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
@@ -28,6 +36,7 @@ const changeDirection = (event) => {
 }
 
 const initGame = () => {
+    if (gameOver) return handleGameOver();
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
     if(snakeX === foodX && snakeY === foodY) {
@@ -44,6 +53,10 @@ const initGame = () => {
     snakeX += velocityX;
     snakeY += velocityY;
 
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        gameOver = true;
+    } 
+
     for (let i = 0; i < snakeBody.length; i++) {
         htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
     }
@@ -52,5 +65,5 @@ const initGame = () => {
 }
 
 changeFoodPosition();
-setInterval(initGame, 125);
+setIntervalId = setInterval(initGame, 125);
 document.addEventListener("keydown", changeDirection);
